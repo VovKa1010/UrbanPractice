f65a31426497cae352e5384d09b3ea84d1acdde8
vovka <@vovka>
2024-11-18 16:41:59 +0300

    add solution for program organization and string methods practice

---
+++ model_1/program_organization_and_string_methods.py
@@ -0,0 +1,4 @@
+my_string = input("Введите произвольный текст: ")
+
+print(len(my_string), my_string.upper(), my_string.lower(), my_string.replace(" ", ""),
+      my_string[1], my_string[-1], sep="\n")
