87af2bdd2fe19f3ff6575f9b62e659d79453334b
vovka <@vovka>
2024-11-18 16:11:25 +0300

    add solution for variables practice

---
+++ model_1/variables.py
@@ -0,0 +1,7 @@
+count_completed_tasks = 12
+count_hours = 1.5
+course_name = "Python"
+hours_on_task = count_hours / count_completed_tasks
+
+print(f"{course_name}, всего задач: {count_completed_tasks}, затрачено часов: {count_hours}, "
+      f"среднее время выполнения {hours_on_task} часа.")
