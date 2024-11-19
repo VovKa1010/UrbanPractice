dfad8ecbfa6c6fbe63122557917a803ec0be092e
vovka <@vovka>
2024-11-19 16:00:06 +0300

    correction for dictionaries and sets practice

--- model_1/dictionaries_and_sets.py
+++ model_1/dictionaries_and_sets.py
@@ -4,19 +4,22 @@ my_dict = {
     "Dima": 19
 }
 
-print(my_dict, my_dict["Tom"], my_dict.get("Fed"), sep="\n")
+print(my_dict)
+print(my_dict["Tom"])
+print(my_dict.get("Fed"))
 
 my_dict.update({
     "Tera": 4.543,
     "Lover": 50
 })
 
-print(my_dict.pop("Tom"), my_dict, sep="\n")
-
+print(my_dict.pop("Tom"))
+print(my_dict)
 
 my_set = {1, 5, 2, 4, 1, 5, 1, 6, 8, "g", "r", "r", "y"}
 
-print("\n\n", my_set, sep="")
+print("\n")
+print(my_set)
 
 my_set.update({7, "h"})
 
