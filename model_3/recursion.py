43c9681ffbe247b36993c5b36492a999fdb583a9
vovka <@vovka>
2024-11-29 13:52:33 +0300

    correction solution for recursion practice

--- model_3/recursion.py
+++ model_3/recursion.py
@@ -1,14 +1,18 @@
-def get_multiplied_digits(number) -> int:
+def get_multiplied_digits(number: int) -> int:
     str_number = str(number)
     first = int(str_number[0])
 
-    if len(str_number) > 1:
-        first *= get_multiplied_digits(int(str_number[1:]))
+    if len(str_number) > 1 and (other := int(str_number[1:])) != 0:
+        first *= get_multiplied_digits(other)
 
     return first
 
 
 result = get_multiplied_digits(40203)
+print(result)
+result = get_multiplied_digits(0)
+print(result)
+result = get_multiplied_digits(4020300000)
 print(result)
 result = get_multiplied_digits(423)
 print(result)
