74bf20f045ef267ae4c8dfa376372639418c6afc
vovka <@vovka>
2024-12-17 19:20:40 +0300

    add solution for installation of examination, basic_data_structures practice model 0

---
+++ model_0/basic_data_structures.py
@@ -0,0 +1,20 @@
+# 1st program
+print("1st program:", 9 ** 0.5 * 5)
+
+
+# 2nd program
+print("2nd program:", 9.99 > 9.98 and 1000.0 != 1000.1)
+
+
+# 3rd program
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+
+print(f"""3rd program 1: {a} 
+3rd program 2: {b}
+3rd program 3: {a == b}""")
+
+
+# 4th program
+str1 = "123.456"
+print("4th program:", int(float(str1) // 0.1 % 10))
